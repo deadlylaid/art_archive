66cb199d97f31f179b4bc3d7fb49b5cbbde24612
yongdoree <yongdoree@gmail.com>
2016-04-26 22:18:38 +0900

    modify: defined 1:N relationship between images and artists

--- art_archive_project/api/images/models.py
+++ art_archive_project/api/images/models.py
@@ -1,4 +1,5 @@
 from api import db
+from api.artists.models import Artist
 
 
 class Image(db.Model):
@@ -8,14 +9,18 @@ class Image(db.Model):
     image_url = db.Column(db.String(255))
     title = db.Column(db.String(255))
     year = db.Column(db.Integer)
-    artist_id = db.Column(db.Integer)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
+    artist = db.relationship(
+        Artist,
+        backref=db.backref('images', lazy='dynamic'),
+    )
     description = db.Column(db.String(255))
 
-    def __init__(self, title=None, image_url=None, **image_info):
+    def __init__(self, title=None, image_url=None, artist=None, **image_info):
         self.image_url = image_url
         self.title = title
         self.year = image_info.get('year', None)
-        self.artist_id = image_info.get('artist_id', None)
+        self.artist = artist
         self.description = image_info.get('description', None)
 
     def __repr__(self):
