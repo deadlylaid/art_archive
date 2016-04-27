a0cbd189153d3d8f7f5b43a6f676e563064c614a
yongdoree <yongdoree@gmail.com>
2016-04-27 18:35:47 +0900

    modify: artists_detail endpoint according to api documentation

--- art_archive_project/api/artists/controllers/artists_detail.py
+++ art_archive_project/api/artists/controllers/artists_detail.py
@@ -4,18 +4,21 @@ from flask import request
 
 from api.artists.models import Artist
 from api.artists.controllers import artists_api
+from api.utils.json_decorator import json
 
 
 @artists_api.route("/<int:artist_id>/")
+@json
 def artists_detail(artist_id):
-    artist = Artist.query.get_or_404(artist_id)
-    data = {}
-    data['id'] = artist.id
-    data['name'] = artist.name
-    data['birth_year'] = artist.birth_year
-    data['death_year'] = artist.death_year
-    data['country'] = artist.country
-    data['genre'] = artist.genre
-    data['artworks_href'] = "Not implemented yet"
-    artists_response = {"meta": {"response_code": 200}, "data": data}
-    return jsonify(artists_response)
+    artist = Artist.query.get(artist_id)
+    if artist:
+        data = {}
+        data['id'] = artist.id
+        data['name'] = artist.name
+        data['birth_year'] = artist.birth_year
+        data['death_year'] = artist.death_year
+        data['country'] = artist.country
+        data['genre'] = artist.genre
+        data['artworks_href'] = "Not implemented yet"
+        return {"data": data}, 200
+    return {"error": "URL path invalid, check artist_id"}, 404
