e63f9b3ccc964d204eb2ccc7d86dbb2a4fc55701
yongdoree <yongdoree@gmail.com>
2016-04-30 23:48:04 +0900

    refactor: artists_list_post endpoint

--- art_archive_project/api/artists/controllers/artists/artists_list_post.py
+++ art_archive_project/api/artists/controllers/artists/artists_list_post.py
@@ -1,13 +1,11 @@
 from flask import request
 
 from api import db
-from api.artists.models import Artist
 from api.artists.controllers import artists_api
 from api.utils.json_decorator import json
-from api.utils.response_wrapper import ok_response, created_response
-from api.utils.errors import unprocessable_entry
-from api.utils.url_helper import get_absolute_url
 from api.utils.nullify import nullify
+from api.utils.errors import unprocessable_entry
+from api.utils.create_new_artist import create_new_artist
 
 
 @artists_api.route("/", methods=['POST'])
@@ -22,13 +20,4 @@ def artists_list_post():
     }
     if not ('name' in params and 'country' in params and 'genre' in params):
         return unprocessable_entry("name, country, genre are required parameters")
-    new_artist = Artist(**params)
-    db.session.add(new_artist)
-
-    try:
-        db.session.commit()
-        data = new_artist.to_json
-        data['artworks_href'] = get_absolute_url('artists_api.artists_artworks', artist_id=new_artist.id)
-        return created_response(data)
-    except Exception:
-        return unprocessable_entry("name, country, genre should be string, birth_year, death_year should be integer")
+    return create_new_artist(db, **params)
